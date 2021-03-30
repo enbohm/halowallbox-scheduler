80ed69ada4bef2fa3a8c29f926137c14989b2baf
enbohm <andreas@enbohms.se>
2021-03-30 10:13:20 +0200

    Updating halo client params

--- src/main/java/se/enbohms/halo/restclients/HaloRestClient.java
+++ src/main/java/se/enbohms/halo/restclients/HaloRestClient.java
@@ -4,6 +4,7 @@ import javax.ws.rs.HeaderParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
@@ -16,9 +17,9 @@ public interface HaloRestClient {
   Response getAuthToken(@HeaderParam("apiKey") String apiKey, HaloLoginPayload loginPayload);
 
   @PUT
-  @Path("/chargepoints/2005010779M/settings")
+  @Path("/chargepoints/{wallboxId}/settings")
   Response changeSettings(@HeaderParam("Authorization") String apiToken,
-      HaloSettingsPayload settingsPayload);
+      @PathParam("wallboxId") String wallboxId, HaloSettingsPayload settingsPayload);
 
 
 }
