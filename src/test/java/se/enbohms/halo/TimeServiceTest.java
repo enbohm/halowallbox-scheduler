9351ff6647ec4a442be6dd8c619339ee221557eb
enbohm <andreas@enbohms.se>
2021-04-09 07:08:03 +0200

    Updating testcase

--- src/test/java/se/enbohms/halo/TimeServiceTest.java
+++ src/test/java/se/enbohms/halo/TimeServiceTest.java
@@ -1,23 +1,25 @@
 package se.enbohms.halo;
 
-import static org.wildfly.common.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusTest;
+
 import java.time.LocalTime;
 import javax.inject.Inject;
+
 import org.junit.jupiter.api.Test;
 
 @QuarkusTest
 @QuarkusTestResource(WiremockTimeServiceEndpoint.class)
 public class TimeServiceTest {
 
-  @Inject
-  TimeService timeService;
+    @Inject
+    TimeService timeService;
 
-  @Test
-  void should_return_a_local_sunset_time() {
-    LocalTime time = timeService.getSunsetTime();
-    assertNotNull(time);
-  }
+    @Test
+    void should_return_a_local_sunset_time() {
+        LocalTime result = timeService.getSunsetTime();
+        assertNotNull(result, () -> "Time should not be null");
+    }
 }
