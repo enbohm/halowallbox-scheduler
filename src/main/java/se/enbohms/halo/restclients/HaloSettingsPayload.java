c00046a672910254a16b8ddd7003891017bc09d9
enbohm <andreas@enbohms.se>
2021-09-27 16:05:01 +0200

    Initial resources for Java 17

--- src/main/java/se/enbohms/halo/restclients/HaloSettingsPayload.java
+++ src/main/java/se/enbohms/halo/restclients/HaloSettingsPayload.java
@@ -3,27 +3,5 @@ package se.enbohms.halo.restclients;
 import io.quarkus.runtime.annotations.RegisterForReflection;
 
 @RegisterForReflection
-public class HaloSettingsPayload {
-
-  private String id;
-  private String dimmer;
-  private String downlight;
-
-  public HaloSettingsPayload(String id, String dimmer, String downlight) {
-    this.id = id;
-    this.dimmer = dimmer;
-    this.downlight = downlight;
-  }
-
-  public String getId() {
-    return id;
-  }
-
-  public String getDimmer() {
-    return dimmer;
-  }
-
-  public String getDownlight() {
-    return downlight;
-  }
+public record HaloSettingsPayload(String id, String dimmer, String downLight) {
 }
