d3b4dcaf65123342aedfbfb6035bb88612bdae52
enbohm <andreas@enbohms.se>
2021-10-12 07:51:39 +0200

    Fixing setting payload sent to Halow backend

--- src/main/java/se/enbohms/halo/restclients/HaloSettingsPayload.java
+++ src/main/java/se/enbohms/halo/restclients/HaloSettingsPayload.java
@@ -1,7 +1,16 @@
 package se.enbohms.halo.restclients;
 
 import io.quarkus.runtime.annotations.RegisterForReflection;
+import se.enbohms.halo.PrivateVisibilityStrategy;
+
+import javax.json.bind.annotation.JsonbCreator;
+import javax.json.bind.annotation.JsonbVisibility;
 
 @RegisterForReflection
+@JsonbVisibility(value = PrivateVisibilityStrategy.class)
 public record HaloSettingsPayload(String id, String dimmer, String downLight) {
+
+    @JsonbCreator
+    public HaloSettingsPayload {
+    }
 }
